d02cde3c98e13bf2cd33c5fa66556c95d9756832
Brian Faust <hello@basecode.sh>
2020-08-27 05:25:26 +0300

    wip

--- __tests__/server/utils.test.ts
+++ __tests__/server/utils.test.ts
@@ -5,27 +5,27 @@ import { getBaseTransactionId } from "../../packages/server/src/server/utils";
 import { multisigAsset, passphrase, passphrases, publicKey } from "../__mocks__/transaction";
 
 beforeAll(async () => {
-    Managers.configManager.setFromPreset("testnet");
-    Managers.configManager.setHeight(2); // aip11 from height 2
+	Managers.configManager.setFromPreset("testnet");
+	Managers.configManager.setHeight(2); // aip11 from height 2
 });
 
 describe("Utils", () => {
-    describe("getBaseTransactionId", () => {
-        it("should return the base transaction id", () => {
-            const transactionBuilder = Transactions.BuilderFactory.multiSignature()
-                .multiSignatureAsset(multisigAsset)
-                .network(23)
-                .senderPublicKey(publicKey);
+	describe("getBaseTransactionId", () => {
+		it("should return the base transaction id", () => {
+			const transactionBuilder = Transactions.BuilderFactory.multiSignature()
+				.multiSignatureAsset(multisigAsset)
+				.network(23)
+				.senderPublicKey(publicKey);
 
-            const baseTransactionId = Transactions.Utils.getId(transactionBuilder.data);
+			const baseTransactionId = Transactions.Utils.getId(transactionBuilder.data);
 
-            const transaction = transactionBuilder
-                .multiSign(passphrase, 0)
-                .multiSign(passphrases[1], 1)
-                .sign(passphrase)
-                .getStruct();
+			const transaction = transactionBuilder
+				.multiSign(passphrase, 0)
+				.multiSign(passphrases[1], 1)
+				.sign(passphrase)
+				.getStruct();
 
-            expect(getBaseTransactionId(transaction)).toBe(baseTransactionId);
-        });
-    });
+			expect(getBaseTransactionId(transaction)).toBe(baseTransactionId);
+		});
+	});
 });
